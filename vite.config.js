5e54ca8beb0efb915a215c2179c206cc53984ecd
Azra <133489880+azrairemd@users.noreply.github.com>
2026-07-10 14:53:59 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-})
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/myPortfolio/",  
+  base: "/myPortfolio/",
 })
